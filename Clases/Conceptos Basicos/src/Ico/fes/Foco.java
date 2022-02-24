3fa9d22f7537d304710abe11eac069c0f4b75f74
Didier05 <didierbecerra61@aragon.unam.mx>
2022-02-24 12:40:47 -0600

    Iniciamos el archivo Foco.java

---
+++ Clases/Conceptos Basicos/src/Ico/fes/Foco.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ico.fes;
+
+/**
+ *
+ * @author Marely Medina
+ */
+public class Foco {
+    private String colo;
+    private String tipo;
+    private int tamano;
+
+    public Foco() {
+    }
+
+    public Foco(String colo, String tipo, int tamano) {
+        this.colo = colo;
+        this.tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getColo() {
+        return colo;
+    }
+
+    public void setColo(String colo) {
+        this.colo = colo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
