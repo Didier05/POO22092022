5d5e6901779d3bc93ebf3bc549a8acdcf259e61b
Didier05 <didierbecerra61@aragon.unam.mx>
2022-02-26 16:42:26 -0600

    Primer commit de tarea

--- Tareas/Libro/src/libro/Libro.java
+++ Tareas/Libro/src/libro/Libro.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tarea.libro;
+package libro;
 
 /**
  *
  * @author Marely Medina
  */
-public class TareaLibro {
+public class Libro {
 
     /**
      * @param args the command line arguments
