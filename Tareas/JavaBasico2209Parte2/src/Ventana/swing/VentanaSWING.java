a7912745b07fef299275528889fe58089de2884f
Didier05 <didierbecerra61@aragon.unam.mx>
2022-05-12 13:02:14 -0500

    Clase Ventana Swing

---
+++ Tareas/JavaBasico2209Parte2/src/Ventana/swing/VentanaSWING.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ventana.swing;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Marely Medina
+ */
+public class VentanaSWING extends JFrame {
+private JTextField Cuadro;
+private JButton Boton;
+private JLabel Resultado;
+    
+    public VentanaSWING() throws HeadlessException {
+        setTitle("Conversion de °C   a  °F");
+        setSize(400, 300);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        Cuadro = new JTextField(5);  
+        ImageIcon icono = new ImageIcon(System.getProperty("user.dir")+"/src/ico/fes/swing/carro.png");        
+        Boton  = new JButton(icono);
+        Boton.setBackground(Color.GREEN);
+        Boton.setOpaque(true);
+        Boton.setToolTipText("Click para convertir en °F");
+        Resultado = new JLabel("°F");
+        this.getContentPane().add(Cuadro);
+        this.getContentPane().add(Boton);
+        this.getContentPane().add(Resultado);
+        this.setVisible(true);
+        
+        this.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+          System.exit(0);
+            }
+        });
+        
+        this.Boton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            float gradosFarenheit=0.0f;
+            float Celsius=0.0f;
+                try {
+                    Celsius= Float.parseFloat(Cuadro.getText());
+                    gradosFarenheit= (Celsius *(9.0f/5.0f))+32.0f;
+                    Resultado.setText(gradosFarenheit + "°F");
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "INtroduce un valor Númerico", "Error de captira !!!", JOptionPane.ERROR_MESSAGE);
+                }
+                
+            }
+            
+            
+        });
+        
+        
+    }
+    
+    
+}
--- Tareas/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
+++ Tareas/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -6,6 +6,7 @@ package javabasico2209parte2;
 
 import Ico.fes.awt.Ventana;
 import Ico.fes.awt.VentanaVersion2;
+import Ventana.swing.VentanaSWING;
 
 /**
  *
@@ -14,6 +15,9 @@ import Ico.fes.awt.VentanaVersion2;
 public class JavaGraficoMain {
     public static void main(String[] args) { //main mas control y espacio
         //Ventana ventana= new Ventana("Hola AWT ICO FES Aragon");
-        VentanaVersion2 ventana = new VentanaVersion2();
+        //VentanaVersion2 ventana = new VentanaVersion2();
+          VentanaSWING vs = new VentanaSWING();
+          System.out.println(System.getProperty("user.dir")); 
+                
     }
 }
